1cf8e146b166769b7683ad334a8b157c901a15c6
Tyler Acosta <tacos1998@gmail.com>
2016-03-02 14:53:31 +0000

    MIRACLE fix https in activation email link

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
   host = 'rails-tutorial-tacos1998.c9users.io'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log

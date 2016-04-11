5a1f4230933fcdfc43b26fcc9035ef2d9c9d6920
Tyler Acosta <tacos1998@gmail.com>
2016-04-11 02:29:00 +0000

    remove unactivated users from Nibble recipients and remove optional description from Nibble subjects

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,7 +1,7 @@
 module MessagesHelper
   def recipients_options(chosen_recipient = nil)
     s = ''
-    User.all.each do |user|
+    User.where(activated: true).each do |user|
       s << "<option value='#{user.id}' data-img-src='#{gravatar_image_url(user.email, size: 50)}' #{'selected' if user == chosen_recipient}>#{user.name}</option>"
     end
     s.html_safe

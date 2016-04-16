73974ce78b641b3760add2f88b8eaa13a2323b7c
Tyler Acosta <tacos1998@gmail.com>
2016-04-16 22:07:54 +0000

    add link but not hover

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,6 +5,10 @@ module UsersHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    if current_user?(user)
+      image_tag(gravatar_url, class: "gravatar", href: "https://en.gravatar.com/site/login", id: "change-Gravatar", target: "_blank")
+    else
+      image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
   end
 end
